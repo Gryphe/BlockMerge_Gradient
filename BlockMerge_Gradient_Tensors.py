09d12315940ef6aef3fab50723735543a14d0932
Gryphe <30298625+Gryphe@users.noreply.github.com>
2023-08-06 08:52:03 +0200

    Update BlockMerge_Gradient_Tensors.py

--- BlockMerge_Gradient_Tensors.py
+++ BlockMerge_Gradient_Tensors.py
@@ -50,7 +50,7 @@ def clear_console():
         subprocess.call("clear", shell=True)
         
 
-def merge_models(model1, model2, gradient_values, layer_only=False):
+def merge_models(model1, model2, gradient_values, layer_only=False, no_layers=False):
     """
     Merge two models by blending their state_dicts based on a smoothly interpolated list of gradient values.
 
@@ -71,8 +71,28 @@ def merge_models(model1, model2, gradient_values, layer_only=False):
         # Filter keys if layer_only is True
         if layer_only:
             keys = [key for key in state_dict1.keys() if "layer" in key]
+        elif no_layers:
+            keys = [key for key in state_dict1.keys() if "layer" not in key]
         else:
             keys = state_dict1.keys()
+            
+        # Function to merge tensors when vocab sizes differ
+        def merge_vocab_tensors(tensor1, tensor2, blend_ratio):
+            vocab_size1 = tensor1.shape[0]
+            vocab_size2 = tensor2.shape[0]
+            min_vocab_size = min(vocab_size1, vocab_size2)
+        
+            # Create a new tensor based on the vocab size of model1
+            new_tensor = torch.zeros(vocab_size1, tensor1.shape[1], dtype=tensor1.dtype).to(tensor1.device)
+        
+            # Blend the values for the common vocab size
+            new_tensor[:min_vocab_size, :] = tensor1[:min_vocab_size, :] * (1 - blend_ratio) + tensor2[:min_vocab_size, :] * blend_ratio
+        
+            # If there are unique tokens in tensor1, retain their original values
+            if vocab_size1 > min_vocab_size:
+                new_tensor[min_vocab_size:, :] = tensor1[min_vocab_size:, :]
+        
+            return new_tensor    
 
         # Calculate the sections based on gradient values
         sections = len(gradient_values) - 1
@@ -96,8 +116,12 @@ def merge_models(model1, model2, gradient_values, layer_only=False):
             ratio_model2 = blend_ratios[idx]
             ratio_model1 = 1 - ratio_model2
 
-            # Blend the tensors using the blend ratios
-            state_dict1[key] = (ratio_model1 * state_dict1[key] + ratio_model2 * state_dict2[key])
+            # If the tensor is one of those with differing vocab sizes
+            if key in ["lm_head.weight", "model.embed_tokens.weight"]:
+                state_dict1[key] = merge_vocab_tensors(state_dict1[key], state_dict2[key], ratio_model2)
+            else:
+                # Blend the tensors using the blend ratios
+                state_dict1[key] = (ratio_model1 * state_dict1[key] + ratio_model2 * state_dict2[key])
 
             # Print log of blending ratios for current tensor
             print(f"{datetime.now().strftime('%H:%M:%S')} - Merging tensor {key} ({idx}/{len(keys)}) ({round(ratio_model1, 2)} - {round(ratio_model2, 2)})")
@@ -136,7 +160,7 @@ def main(args):
 
         # Merge the models
         print(f"{datetime.now().strftime('%H:%M:%S')} - Merging models...")
-        merge_models(model1, model2, args.gradient_values, args.layer_only)
+        merge_models(model1, model2, args.gradient_values, args.layer_only, args.no_layers)
 
         if args.output_model_path:
             print(f"{datetime.now().strftime('%H:%M:%S')} - Saving new model...")
@@ -172,6 +196,7 @@ if __name__ == "__main__":
     parser.add_argument('--gradient_values', type=literal_eval, required=True, help='List of gradient values. e.g. [1.0, 0.5, 0.0]')
     parser.add_argument('--max_shard_size', type=str, default="2000MiB", help='Output shard size')
     parser.add_argument('--layer_only', action='store_true', help='If set, only process tensors with keys containing "layer"')
+    parser.add_argument('--no_layers', action='store_true', help='If set, only process tensors with keys NOT containing "layer"')
     
     args = parser.parse_args()
     main(args)
